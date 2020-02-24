cd759b4d461e83031a35b0fa246f550e3145fe72
kevinmaiden7 <kevin.m.gallego@gmail.com>
2020-02-24 12:25:12 -0500

    Configurando index.js y package.json

--- functions/index.js
+++ functions/index.js
@@ -22,13 +22,5 @@ const functions = require('firebase-functions');
 // Instantiate the Dialogflow client.
 const app = dialogflow({debug: true});
 
-// Handle the Dialogflow intent named 'favorite color'.
-// The intent collects a parameter named 'color'.
-app.intent('favorite color', (conv, {color}) => {
-    const luckyNumber = color.length;
-    // Respond with the user's lucky number and end the conversation.
-    conv.close('Your lucky number is ' + luckyNumber);
-});
-
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
